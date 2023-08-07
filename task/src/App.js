f61c6d54a349d42c814ea2d480cd1be248b6c95e
maria.pinto <maria.pinto@presidencia.gov.br>
2023-08-07 13:10:49 -0300

    primeiro projeto em react

---
+++ task/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import {useState, useEffect} from "react";
+import {BsTrash, BsBookmarkCheck, BsBookmarkCheckFill} from "react-icons/bs";
+
+const API = "https://localhost:5000"
+
+function App() {
+  return (
+    <div className="App">
+    <h1> Task</h1>
+    </div>
+  );
+}
+
+export default App;
